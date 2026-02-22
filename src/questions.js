330415a5362180ffaa50f141abd2c4a21cf4beae
dj-pmfst <djuric@pmfst.hr>
2026-02-22 21:00:56 +0100

    add questions

--- src/questions.js
+++ src/questions.js
@@ -1,102 +1,102 @@
 export const questions = [
     {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "In the original Jurrasic park, which species are the raptors based on?",
+        answers: ["Velociraptor", "Deinonychus", "Utahraptor", "Deionsuchus"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
+        question: "How many engines does the commerical airplaine Airbus A380 have?",
+        answers: ["1", "2", "3", "4"],
+        correct: 3
+      },
+      {
+        question: "What is the spin of an electron?",
+        answers: ["1/2", "1", "3/2", "2"],
+        correct: 0
+      },
+      {
+        question: "Which film won the Best Picture in 92nd Academy Awards?",
+        answers: ["Black Swan", "Arrival", "Parasite", "Life of Pi"],
         correct: 2
       },
       {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "Which of these is not a pokemon?",
+        answers: ["Sentret", "Altaria", "Absol", "Decadron"],
+        correct: 3
+      },
+      {
+        question: "In what year was the FIFA World Cup hosted by Russia held?",
+        answers: ["2014", "2018", "2006", "2019"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
+        question: "How long did it take the Apollo 11 mission to reach the moon?",
+        answers: ["12h", "22h", "2 days", "3 days"],
+        correct: 3
+      },
+      {
+        question: "What constellation contains the star Antares?",
+        answers: ["Corona borealis", "Libra", "Scorpio", "Ursa major"],
         correct: 2
       },
       {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "Which of these is a dinosaur?",
+        answers: ["Deltadromeus", "Pteranodon", "Mosasaurus", "Quetzcoatl"],
+        correct: 0
+      },
+      {
+        question: "The kinetic part of the Schrödinger equation contains a spatial derivative of which order?",
+        answers: ["1", "2", "3", "4"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
-      },
-      {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "Which video game was the first to offer a DLC?",
+        answers: ["Halo 3", "The Elder Scrolls 4: Oblivion", "Fortnite", "Call of Duty: Black Ops"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
-      },
-      {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "How many main stars does the Ursa major (also known as Big Dipper) have?",
+        answers: ["5", "7", "6", "10"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
+        question: "Which of these countries doesn't have mosquitoes?",
+        answers: ["Hawaii", "New Zealand", "Norway", "Iceland"],
+        correct: 3
+      },
+      {
+        question: "Who is the author of the famous modernist novel Ulysses?",
+        answers: ["Fyodor Dostoevsky", "J. D. Salinger", "James Joyce", "Ernest Hemingway"],
         correct: 2
       },
       {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "What is the correct quote of the famous revelation in Star Wars: The Empire Strikes Back, which Darth Vader tells Luke Skywalker?",
+        answers: ["Luke, I am your father.", "No, I am your father.", "I am your father.", "No, Luke, I am your father."],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
+        question: "What is considered to be the first video game ever created?",
+        answers: ["Snake", "Pac-Man", "Tennis for Two", "Pong"],
         correct: 2
       },
       {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "How many official languages does Switzerland have?",
+        answers: ["1", "2", "3", "4"],
+        correct: 3
+      },
+      {
+        question: "How many sides does an icosahedron have?",
+        answers: ["16", "12", "20", "8"],
+        correct: 2
+      },
+      {
+        question: "What was the final result of the Brazil v Germany semi-final during the 2014 FIFA World Cup?",
+        answers: ["2-5", "1-7", "2-0", "3-1"],
         correct: 1
       },
       {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
-      },
-      {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
+        question: "In what year was the film Star Wars: Attack of the Clones released?",
+        answers: ["1999", "2002", "1986", "2008"],
         correct: 1
-      },
-      {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
-      },
-      {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
-        correct: 1
-      },
-      {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
-      },
-      {
-        question: "What is the capital of France?",
-        answers: ["Berlin", "Paris", "Madrid", "Rome"],
-        correct: 1
-      },
-      {
-        question: "How many sides does a hexagon have?",
-        answers: ["5", "7", "6", "8"],
-        correct: 2
       }
 ]
